c9ecfd51f5ffb89f042fe927631800bc6409688b
BT_1E10_17 <sample@bths.edu>
2022-04-05 13:23:59 -0400

    more things2

---
+++ src/Countdown.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Countdown
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number to start: ");
+        int start = input.nextInt();
+        System.out.println(countdown(start));
+    }
+
+    public static String countdown(int number)
+    {
+        if (number == 0)
+        {
+            return "Blastoff!";
+        }
+        else
+        {
+            return countdown(number - 1) + number;
+        }
+    }
+}
